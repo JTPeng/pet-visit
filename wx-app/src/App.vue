d2e0a66adf6966f5c33f27e3d072617d05d70796
JTPeng <404502753@qq.com>
2026-05-11 15:28:34 +0800

    feat(wx-app): add silent login on App launch

--- wx-app/src/App.vue
+++ wx-app/src/App.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
-onLaunch(() => {
-  console.log("App Launch");
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app';
+import { useUserStore } from './stores/user';
+import { getToken } from './api/request';
+
+onLaunch(async () => {
+  const userStore = useUserStore();
+  const token = getToken();
+
+  if (token) {
+    userStore.token = token;
+    await userStore.fetchMe();
+  } else {
+    await userStore.login();
+  }
 });
-onShow(() => {
-  console.log("App Show");
-});
-onHide(() => {
-  console.log("App Hide");
-});
+
+onShow(() => {});
+onHide(() => {});
 </script>
 <style></style>
